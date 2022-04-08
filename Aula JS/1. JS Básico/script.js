fde9b02fcbd75bbb8df99559b1050d80975a91b7
Gabriel <gabriel.codeman@gmail.com>
2022-04-08 18:15:55 -0300

    Update script.js

--- Aula JS/1. JS Básico/script.js
+++ Aula JS/1. JS Básico/script.js
@@ -1,3 +1,11 @@
-var caixa = "vape"; 
+// OPERADORES LÓGICOS
+var idade = 70;
 
-alert(caixa);
+var menor10 = idade <= 10;
+var maior65 = idade >=65;
+
+var gratuidade = menor10 || maior65;    
+
+console.log("Idade ", idade);
+console.log ("Maior que 65 ", maior65);
+console.log("Menor que 10: ", menor10);
