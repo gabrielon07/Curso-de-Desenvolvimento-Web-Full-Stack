815b6d358448d40d3f25ba1195a90d5ebe2f991c
Gabriel <gabriel.codeman@gmail.com>
2022-04-08 20:02:45 -0300

    Update script.js

--- Aula JS/1. JS Básico/script.js
+++ Aula JS/1. JS Básico/script.js
@@ -1,4 +1,6 @@
-// OPERADORES LÓGICOS
+// 1 - OPERADORES LÓGICOS
+
+/*
 var idade = 70;
 
 var menor10 = idade <= 10;
@@ -8,4 +10,28 @@ var gratuidade = menor10 || maior65;
 
 console.log("Idade ", idade);
 console.log ("Maior que 65 ", maior65);
-console.log("Menor que 10: ", menor10);
+console.log("Menor que 10: ", menor10); 
+
+********************************************************************************************
+*/
+
+
+/*
+// 2 - TRANSFORMANDO STRINGS EM NÚMEROS 
+
+// transformando em numero int:
+var a = "3";
+var b = "5";
+var c = parseInt(a) + parseInt(b);
+
+console.log(c)
+
+// transformando em numero float:
+var d = "3.5";
+var e = "5";
+var f = parseFloat(d) + parseInt (e);
+
+console.log(f); 
+
+************************************************************************************************
+*/
